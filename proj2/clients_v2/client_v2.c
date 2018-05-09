5ed83ee2609a8f4a6c32e9b02371704fea4d8743
GNeves95 <up201306485@fe.up.pt>
2018-05-09 16:42:48 +0100

    Add files via upload

--- proj2/clients_v2/client_v2.c
+++ proj2/clients_v2/client_v2.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
   printf("** Running process %d (PGID %d) **\n", getpid(), getpgrp());
 
+  char str[99];
+
+  sprintf(str, "/tmp/ans%d", getpid());
+  printf("%s\n", str);
+
   if (argc == 4)
     printf("ARGS: %s | %s | %s\n", argv[1], argv[2], argv[3]);
 
   sleep(1);
+
+  mkfifo(str,0660);
 
   return 0;
 }
